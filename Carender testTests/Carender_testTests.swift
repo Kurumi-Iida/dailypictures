0dba41009c155cc85470a5e08c1ce5892d23d23e
飯田くるみ <iidakurumi@iidakurumi-no-macbook-pro.local>
2015-12-28 14:23:28 +0900

    Initial Commit

---
+++ Carender testTests/Carender_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  Carender_testTests.swift
+//  Carender testTests
+//
+//  Created by 飯田くるみ on 2015/12/28.
+//  Copyright © 2015年 Kurumi Iida. All rights reserved.
+//
+
+import XCTest
+@testable import Carender_test
+
+class Carender_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
