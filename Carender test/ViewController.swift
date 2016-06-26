ab08ff24df5f2f8d448021c79e2e153503f26fa3
飯田くるみ <iidakurumi@iidakurumi-no-macbook-pro.local>
2016-06-26 16:36:59 +0900

    calendarを他のviewに分離

--- Carender test/ViewController.swift
+++ Carender test/ViewController.swift
@@ -22,513 +22,53 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     // チェックボックス
     //@IBOutlet var button: UIButton!
     
-    //メンバ変数の設定（カレンダー用）
-    var now: NSDate!
-    var year: Int!
-    var month: Int!
-    var day: Int!
-    var Maxday: Int!
-    var dayOFWeek: Int!
-    
-    //メンバ変数の設定（カレンダー関数から取得したものを渡す）
-    var comps: NSDateComponents!
-    
-    //メンバ変数の設定
-    var calendarBackGroundColor: UIColor!
-    var beforecangeBackgroundColor: UIColor!
-    
+    @IBOutlet weak var calendar: CalendarView!
     //プロパティを指定
     @IBOutlet var calendarBar: UILabel!
     @IBOutlet var prevMonthButton: UIButton!
     @IBOutlet var nextMonthButton: UIButton!
     
-    
-    //カレンダーの位置決め用メンバ変数
-    var calendarLabelIntervalX: Int!
-    var calendarLabelX: Int!
-    var calendarLabelY: Int!
-    var calendarLabelWidth: Int!
-    var calendarLabelHeight: Int!
-    var calendarLableFontSize: Int!
-    
-    var buttonRadius: Float!
-    
-    var calendarIntervalX: Int!
-    var calendarX: Int!
-    var calendarIntervalY: Int!
-    var calendarY: Int!
-    var calendarSize: Int!
-    var calendarFontSize: Int!
-    
     var timer: NSTimer!
-    var flag: Bool = true
+    var animationFlag: Bool = true
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        //getAllPhotosInfo()
+        
+        calendar.delegate = self
+        setupCalendarTitle(calendar.year, month: calendar.month)
+
         timer = NSTimer.scheduledTimerWithTimeInterval(4,
             target: self,
             selector: Selector("textRed"),
             userInfo: nil,
             repeats: true
         )
-
         
-        //現在起動中のデバイスを取得（スクリーンの幅・高さ）
-        let screenWidth  = Int(
-            UIScreen.mainScreen().bounds.size.width);
-        let screenHeight =
-        Int(UIScreen.mainScreen().bounds.size.height);
-        
-        //iPhone4s
-        if(screenWidth == 320 && screenHeight == 480){
-            
-            calendarLabelIntervalX = 5;
-            calendarLabelX         = 45;
-            calendarLabelY         = 93;
-            calendarLabelWidth     = 40;
-            calendarLabelHeight    = 25;
-            calendarLableFontSize  = 14;
-            
-            buttonRadius           = 20.0;
-            
-            calendarIntervalX      = 5;
-            calendarX              = 45;
-            calendarIntervalY      = 120;
-            calendarY              = 45;
-            calendarSize           = 40;
-            calendarFontSize       = 17;
-            
-            //iPhone5またはiPhone5s
-        }else if (screenWidth == 320 && screenHeight == 568){
-            
-            calendarLabelIntervalX = 5;
-            calendarLabelX         = 45;
-            calendarLabelY         = 93;
-            calendarLabelWidth     = 40;
-            calendarLabelHeight    = 25;
-            calendarLableFontSize  = 14;
-            
-            buttonRadius           = 20.0;
-            
-            calendarIntervalX      = 5;
-            calendarX              = 45;
-            calendarIntervalY      = 120;
-            calendarY              = 45;
-            calendarSize           = 40;
-            calendarFontSize       = 17;
-            
-            //iPhone6
-        }else if (screenWidth == 375 && screenHeight == 667){
-            
-            calendarLabelIntervalX = 30;//曜日すき間広く
-            calendarLabelX         = 50;//曜日のすき間せまく
-            calendarLabelY         = 95;//曜日高さ
-            calendarLabelWidth     = 50;//曜日
-            calendarLabelHeight    = 50;
-            calendarLableFontSize  = 50;
-            
-            buttonRadius           = 22.5;
-            
-            calendarIntervalX      = 4;//カレンダー横ズレ
-            calendarX              = 53;//カレンダー間
-            calendarIntervalY      = 135;//カレンダー上下
-            calendarY              = 55;//カレンダー上下の間
-            calendarSize           = 50;//ボタンの大きさ
-            calendarFontSize       = 100;
-            
-            self.prevMonthButton.frame = CGRectMake(15, 438, CGFloat(calendarSize), CGFloat(calendarSize));
-            self.nextMonthButton.frame = CGRectMake(314, 438, CGFloat(calendarSize), CGFloat(calendarSize));
-            
-            //iPhone6 plus
-        }else if (screenWidth == 414 && screenHeight == 736){
-            
-            calendarLabelIntervalX = 15;
-            calendarLabelX         = 55;
-            calendarLabelY         = 95;
-            calendarLabelWidth     = 55;
-            calendarLabelHeight    = 25;
-            calendarLableFontSize  = 18;
-            
-            buttonRadius           = 25;
-            
-            calendarIntervalX      = 18;
-            calendarX              = 55;
-            calendarIntervalY      = 125;
-            calendarY              = 55;
-            calendarSize           = 50;
-            calendarFontSize       = 21;
-            
-            self.prevMonthButton.frame = CGRectMake(18, 468, CGFloat(calendarSize), CGFloat(calendarSize));
-            self.nextMonthButton.frame = CGRectMake(348, 468, CGFloat(calendarSize), CGFloat(calendarSize));
-        }
-        
-        //ボタンを角丸にする
-        prevMonthButton.layer.cornerRadius = CGFloat(buttonRadius)
-        nextMonthButton.layer.cornerRadius = CGFloat(buttonRadius)
-        
-        //現在の日付を取得する
-        now = NSDate()
-        
-        //inUnit:で指定した単位（月）の中で、rangeOfUnit:で指定した単位（日）が取り得る範囲
-        let calendar: NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-        let range: NSRange = calendar.rangeOfUnit(NSCalendarUnit.Day, inUnit:NSCalendarUnit.Month, forDate:NSDate())
-        
-        //最初にメンバ変数に格納するための現在日付の情報を取得する
-        comps = calendar.components([
-            NSCalendarUnit.Year,
-            NSCalendarUnit.Month,
-            NSCalendarUnit.Day,
-            NSCalendarUnit.Weekday],
-            fromDate:now);
-    
-        
-        
-        //年月日と最後の日付と曜日を取得(NSIntegerをintへのキャスト不要)
-        let orgYear: NSInteger = comps.year
-        let orgMonth: NSInteger = comps.month
-        let orgDay: NSInteger = comps.day
-        let orgDayOfWeek: NSInteger = comps.weekday
-        let max: NSInteger = range.length
-        
-        year      = orgYear
-        month     = orgMonth
-        day       = orgDay
-        dayOFWeek = orgDayOfWeek
-        Maxday    = max
-        
-        //空の配列を作成する（カレンダーデータの格納用）
-        mArray = NSMutableArray()
-        
-        //曜日ラベル初期定義
-        let monthName:[String] = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
-        
-        //曜日ラベルを動的に配置
-        setupCalendarLabel(monthName)
-        
-        //初期表示時のカレンダーをセットアップする
-        setupCurrentCalendar()
-
         PHPhotoLibrary.requestAuthorization{ handler in
             print("hoge")
         }
-    }
-    
-    //曜日ラベルの動的配置関数
-    func setupCalendarLabel(array: NSArray) {
-        
-        let calendarLabelCount = 7
-        
-        for i in 0...6{
-            
-            //ラベルを作成
-            let calendarBaseLabel: UILabel = UILabel()
-            
-            //X座標の値をCGFloat型へ変換して設定
-            let tmp = i * 3 - 26
-            calendarBaseLabel.frame = CGRectMake(
-                CGFloat(calendarLabelIntervalX + calendarLabelX * (i % calendarLabelCount) + tmp),
-                CGFloat(200 + calendarLabelY),
-                CGFloat(calendarLabelWidth),
-                CGFloat(calendarLabelHeight)
-            )
-            
-            //日曜日の場合は赤色を指定
-            if(i == 0){
-                
-                //RGBカラーの設定は小数値をCGFloat型にしてあげる
-                calendarBaseLabel.textColor = UIColor(
-                    red: CGFloat(0.831), green: CGFloat(0.349), blue: CGFloat(0.224), alpha: CGFloat(1.0)
-                )
-                
-                //土曜日の場合は青色を指定
-            }else if(i == 6){
-                
-                //RGBカラーの設定は小数値をCGFloat型にしてあげる
-                calendarBaseLabel.textColor = UIColor(
-                    red: CGFloat(0.400), green: CGFloat(0.471), blue: CGFloat(0.980), alpha: CGFloat(1.0)
-                )
-                
-                //平日の場合は灰色を指定
-            }else{
-                
-                //既に用意されている配色パターンの場合
-                calendarBaseLabel.textColor = UIColor.lightGrayColor()
-                
-            }
-            
-            //曜日ラベルの配置
-            calendarBaseLabel.text = String(array[i] as! NSString)
-            calendarBaseLabel.textAlignment = NSTextAlignment.Center
-            calendarBaseLabel.font = UIFont(name: "System", size: CGFloat(calendarLableFontSize))
-            self.view.addSubview(calendarBaseLabel)
-        }
-    }
-    
-    //カレンダーを生成する関数
-    func generateCalendar(){
-        
-        //タグナンバーとトータルカウントの定義
-        var tagNumber = 1
-        let total     = 42
-        
-        //7×6=42個のボタン要素を作る
-        for i in 0...41{
-            
-            //配置場所の定義
-            let positionX   = calendarIntervalX + calendarX * (i % 7)
-            let positionY   = 200 + calendarIntervalY + calendarY * (i / 7)
-            let buttonSizeX = calendarSize;
-            let buttonSizeY = calendarSize;
-            
-            //ボタンをつくる
-            let button: UIButton = UIButton()
-            button.frame = CGRectMake(
-                CGFloat(positionX),
-                CGFloat(positionY),
-                CGFloat(buttonSizeX),
-                CGFloat(buttonSizeY)
-            );
-            
-            //ボタンの初期設定をする
-            if(i < dayOFWeek - 1){
-                
-                //日付の入らない部分はボタンを押せなくする
-                button.setTitle("", forState: .Normal)
-                button.enabled = false
-                
-            }else if(i == dayOFWeek - 1 || i < dayOFWeek + Maxday - 1){
-                
-                //日付の入る部分はボタンのタグを設定する（日にち）
-                button.setTitle(String(tagNumber), forState: .Normal)
-                button.tag = tagNumber
-                tagNumber++
-                
-            }else if(i == dayOFWeek + Maxday - 1 || i < total){
-                
-                //日付の入らない部分はボタンを押せなくする
-                button.setTitle("", forState: .Normal)
-                button.enabled = false
-                
-            }
-            
-            //ボタンの配色の設定
-            //@remark:このサンプルでは正円のボタンを作っていますが、背景画像の設定等も可能です。
-            if(i % 7 == 0){
-                button.backgroundColor = UIColor.redColor()
-
-            }else if(i % 7 == 6){
-                button.backgroundColor = UIColor(
-                red: CGFloat(0.2), green: CGFloat(0.3), blue: CGFloat(0.7), alpha: CGFloat(0.7))
-                
-            }else if(i % 7 == 2){
-                
-                button.backgroundColor = UIColor(
-                    red: CGFloat(1), green: CGFloat(0.9), blue: CGFloat(0.8), alpha: CGFloat(1.0))
-                
-            }else if(i % 7 == 3){
-                
-                button.backgroundColor = UIColor(
-                    red: CGFloat(0.8), green: CGFloat(1), blue: CGFloat(1), alpha: CGFloat(1.0))
-                
-            }else if(i % 7 == 1){
-                
-                button.backgroundColor = UIColor(
-                    red: CGFloat(0.8), green: CGFloat(0.9), blue: CGFloat(1), alpha: CGFloat(1.0))
-                
-            }else if(i % 7 == 4){
-                
-                button.backgroundColor = UIColor(
-                    red: CGFloat(0.9), green: CGFloat(1), blue: CGFloat(0.9), alpha: CGFloat(1.0))
-                
-                
-            }else if(i % 7 == 5){
-                
-                button.backgroundColor = UIColor(
-                    red: CGFloat(1), green: CGFloat(1), blue: CGFloat(0.9), alpha: CGFloat(1.0))
-                
-            }
-            
-            calendarBackGroundColor = UIColor.blackColor()
-            
-            //ボタンのデザインを決定する
-            button.setTitleColor(UIColor.blackColor(), forState: .Normal)
-            button.titleLabel!.font = UIFont(name: "System", size: CGFloat(calendarFontSize))
-            button.layer.cornerRadius = CGFloat(buttonRadius)
-            
-            //配置したボタンに押した際のアクションを設定する
-            button.addTarget(self, action: "buttonTapped:", forControlEvents: .TouchUpInside)
-            
-            //ボタンを配置する
-            self.view.addSubview(button)
-            mArray.addObject(button)
-        }
         
     }
-    
-    
-    
-    //タイトル表記を設定する関数
-    func setupCalendarTitleLabel() {
-        calendarBar.text = String("\(year).\(month)")
-    }
-    
-    //現在（初期表示時）の年月に該当するデータを取得する関数
-    func setupCurrentCalendarData() {
-        
-        /*************
-         * (重要ポイント)
-         * 現在月の1日のdayOfWeek(曜日の値)を使ってカレンダーの始まる位置を決めるので、
-         * yyyy年mm月1日のデータを作成する。
-         * 後述の関数 setupPrevCalendarData, setupNextCalendarData も同様です。
-         *************/
-        let currentCalendar: NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-        let currentComps: NSDateComponents = NSDateComponents()
-        
-        currentComps.year  = year
-        currentComps.month = month
-        currentComps.day   = 1
-        
-        let currentDate: NSDate = currentCalendar.dateFromComponents(currentComps)!
-        recreateCalendarParameter(currentCalendar, currentDate: currentDate)
-    }
-    //前の年月に該当するデータを取得する関数
-    func setupPrevCalendarData() {
-        
-        //現在の月に対して-1をする
-        if(month == 0){
-            year = year - 1;
-            month = 12;
-        }else{
-            month = month - 1;
-        }
-        
-        //setupCurrentCalendarData()と同様の処理を行う
-        let prevCalendar: NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-        let prevComps: NSDateComponents = NSDateComponents()
-        
-        prevComps.year  = year
-        prevComps.month = month
-        prevComps.day   = 1
-        
-        let prevDate: NSDate = prevCalendar.dateFromComponents(prevComps)!
-        recreateCalendarParameter(prevCalendar, currentDate: prevDate)
-    }
-    
-    //次の年月に該当するデータを取得する関数
-    func setupNextCalendarData() {
-        
-        //現在の月に対して+1をする
-        if(month == 12){
-            year = year + 1;
-            month = 1;
-        }else{
-            month = month + 1;
-        }
-        
-        //setupCurrentCalendarData()と同様の処理を行う
-        let nextCalendar: NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-        let nextComps: NSDateComponents = NSDateComponents()
-        
-        nextComps.year  = year
-        nextComps.month = month
-        nextComps.day   = 1
-        
-        let nextDate: NSDate = nextCalendar.dateFromComponents(nextComps)!
-        recreateCalendarParameter(nextCalendar, currentDate: nextDate)
-    }
-    
-    //カレンダーのパラメータを再作成する関数
-    func recreateCalendarParameter(currentCalendar: NSCalendar, currentDate: NSDate) {
-        
-        //引数で渡されたものをもとに日付の情報を取得する
-        let currentRange: NSRange = currentCalendar.rangeOfUnit(NSCalendarUnit.Day, inUnit:NSCalendarUnit.Month, forDate:currentDate)
-        
-        comps = currentCalendar.components([NSCalendarUnit.Year, NSCalendarUnit.Month, NSCalendarUnit.Day, NSCalendarUnit.Weekday],fromDate:currentDate)
-        
-        //年月日と最後の日付と曜日を取得(NSIntegerをintへのキャスト不要)
-        let currentYear: NSInteger      = comps.year
-        let currentMonth: NSInteger     = comps.month
-        let currentDay: NSInteger       = comps.day
-        let currentDayOfWeek: NSInteger = comps.weekday
-        let currentMax: NSInteger       = currentRange.length
-        
-        year      = currentYear
-        month     = currentMonth
-        day       = currentDay
-        dayOFWeek = currentDayOfWeek
-        Maxday    = currentMax
-    }
-    
-    //表示されているボタンオブジェクトを一旦削除する関数
-    func removeCalendarButtonObject() {
-        
-        //ビューからボタンオブジェクトを削除する
-        for i in 0..<mArray.count {
-            mArray[i].removeFromSuperview()
-        }
-        
-        //配列に格納したボタンオブジェクトも削除する
-        mArray.removeAllObjects()
-    }
-    
-    //現在のカレンダーをセットアップする関数
-    func setupCurrentCalendar() {
-        
-        setupCurrentCalendarData()
-        generateCalendar()
-        setupCalendarTitleLabel()
-    }
-    
-    //カレンダーボタンをタップした時のアクション
-    func buttonTapped(button: UIButton){
-        
-        //@todo:画面遷移等の処理を書くことができます。
-        
-        //コンソール表示
-        print("\(year)年\(month)月\(button.tag)日が選択されました！")
-        getAllPhotosInfo(year, month: month, day: button.tag)
-        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        button.backgroundColor = calendarBackGroundColor
-        
-     }
-
     
     //前の月のボタンを押した際のアクション
     @IBAction func getPrevMonthData(sender: UIButton) {
-        prevCalendarSettings()
+        calendar.prevCalendarSettings()
     }
     
     //次の月のボタンを押した際のアクション
     @IBAction func getNextMonthData(sender: UIButton) {
-        nextCalendarSettings()
+        calendar.nextCalendarSettings()
     }
     
     //左スワイプで前月を表示
     @IBAction func swipePrevCalendar(sender: UISwipeGestureRecognizer) {
-        prevCalendarSettings()
+        calendar.prevCalendarSettings()
     }
     
     //右スワイプで次月を表示
     @IBAction func swipeNextCalendar(sender: UISwipeGestureRecognizer) {
-        nextCalendarSettings()
-    }
-    
-    //前月を表示するメソッド
-    func prevCalendarSettings() {
-        removeCalendarButtonObject()
-        setupPrevCalendarData()
-        generateCalendar()
-        setupCalendarTitleLabel()
-    }
-    
-    //次月を表示するメソッド
-    func nextCalendarSettings() {
-        removeCalendarButtonObject()
-        setupNextCalendarData()
-        generateCalendar()
-        setupCalendarTitleLabel()
+        calendar.nextCalendarSettings()
     }
     
     private func getNSDateFromString(year: Int, month:Int, day: Int) -> NSDate?{
@@ -581,7 +121,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
             animations: {
                 self.haikeiImageView.alpha = 0.5
             },
-            completion: { flag in
+            completion: { animationFlag in
                 UIView.animateWithDuration(1.5,
                     animations: {
                         self.haikeiImageView.alpha = 1.0
@@ -603,7 +143,6 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
                     }, completion: nil)
         })
     }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -617,4 +156,15 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         }
         
     }
+}
+
+extension ViewController: CalendarViewDelegate {
+    func setupCalendarTitle(year: Int, month: Int) {
+        calendarBar.text = String("\(year).\(month)")
+    }
+    
+    func buttonTapped(year: Int, month: Int, day: Int) {
+        getAllPhotosInfo(year, month: month, day: day)
+    }
+
 }
